7a1cb9ae26551e2c8bea353bb2c28bea35401378
daniilkovall <126925388+daniilkoval11@users.noreply.github.com>
2023-09-07 10:00:37 +0300

    new_commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static double convert(int gallons) {
+        double liters = gallons * 3.78541;
+        return Math.round(liters * 1000.0) / 1000.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(5));
+    }
+}
