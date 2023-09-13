ac092eb657ed43e98096035459fd0b6821f73a10
daniilkoval11 <danilka.koval.2015@mail.ru>
2023-09-13 12:54:53 +0300

    aa

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(convert(5));
-        System.out.println(convert(3));
-        System.out.println(convert(10));
+        System.out.println(convert(5000));
+        System.out.println(convert(36));
+
     }
 }
